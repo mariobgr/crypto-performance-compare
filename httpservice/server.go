da4b947c6fa6655c88f8c445cafa6e3f57b75bfc
mariobgr <mariobgr@icloud.com>
2021-10-18 17:31:50 +0300

    Add untracked files

---
+++ httpservice/server.go
@@ -0,0 +1,39 @@
+package httpservice
+
+import (
+	"crypto-performance-compare/crypto"
+	"crypto-performance-compare/utils"
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type HTTPHandler struct {
+	cache *crypto.Cache
+}
+
+func newHandler(cache *crypto.Cache) HTTPHandler {
+	return HTTPHandler{
+		cache: cache,
+	}
+}
+
+func (h *HTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	res, err := h.cache.Read("BTC")
+	if err != nil {
+		http.Error(w, "no data found for BTC", http.StatusNotFound)
+	}
+
+	json.NewEncoder(w).Encode(res)
+}
+
+func NewServer (logger *log.Logger, cache *crypto.Cache) error {
+	logger.Println("Starting HTTP server on http://localhost" + utils.GetPort())
+
+	handler := newHandler(cache)
+	if err := http.ListenAndServe(utils.GetPort(), &handler); err != nil {
+		return err
+	}
+
+	return nil
+}
